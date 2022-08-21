88d4cb74870ed50ae4c10e7e58dfa777fca3887e
aadii0408 <83656297+aadii0408@users.noreply.github.com>
2022-08-21 21:15:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -148,6 +148,11 @@ def recommend():
         vote_count=vote_count,release_date=release_date,runtime=runtime,status=status,genres=genres,
         movie_cards=movie_cards,reviews=movie_reviews,casts=casts,cast_details=cast_details)
 
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+  return "Hello world!" 
 if __name__ == '__main__':
     app.run(debug=True)
     app.run(host="movie-bluff.herokuapp.com")
